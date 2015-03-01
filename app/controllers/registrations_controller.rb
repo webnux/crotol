f8c222ab8cc557f3abfd60243b343d09b29ea0ac
Pedro Fernandes da Silva <paca.fs@gmail.com>
2015-03-01 21:45:45 +0000

    Edit User Finish

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,10 @@
 class RegistrationsController < Devise::RegistrationsController
-   
+  
+  
   def edit
-     @cover = current_user.facebook.get_object("me?fields=cover")["cover"]["source"]
+    @cover = current_user.facebook.get_object("me?fields=cover")["cover"]["source"]
   end
-   
+
   private
  
   def sign_up_params
@@ -12,6 +13,15 @@ class RegistrationsController < Devise::RegistrationsController
  
   def account_update_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password, :avatar)
+  end
+
+  def update_resource(resource, params)
+    if current_user.provider == "facebook"
+      params.delete("current_password")
+      resource.update_without_password(params)
+    else
+      resource.update_with_password(params)
+    end
   end
    
 
